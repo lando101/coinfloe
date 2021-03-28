18d66a66dd75985f3979d828336e8f0c29c881db
Messmer-Landon <messmer_landon@bah.com>
2021-03-28 15:13:32 -0400

    added components folder and date chips for chart

---
+++ src/app/@shared/components/crypto-chart/crypto-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-chart',
+  templateUrl: './crypto-chart.component.html',
+  styleUrls: ['./crypto-chart.component.scss'],
+})
+export class CryptoChartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -4,18 +4,19 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from '@app/material.module';
 import { LoaderComponent } from './loader/loader.component';
-import { CoinsListsComponent } from './coins-lists/coins-lists.component';
-import { CoinItemComponent } from './coin-item/coin-item.component';
-import { CoinDetailsComponent } from './coin-details/coin-details.component';
+import { CoinsListsComponent } from './components/coins-lists/coins-lists.component';
+import { CoinItemComponent } from './components/coin-item/coin-item.component';
+import { CoinDetailsComponent } from './components/coin-details/coin-details.component';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { BottomSheetCustomComponent } from './bottom-sheet-custom/bottom-sheet-custom.component';
-import { SearchComponent } from './search/search.component';
+import { BottomSheetCustomComponent } from './components/bottom-sheet-custom/bottom-sheet-custom.component';
+import { SearchComponent } from './components/search/search.component';
 import { NgxBootstrapModule } from '@app/ngx-bootstrap.module';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { ProfileMenuComponent } from './profile-menu/profile-menu.component';
-import { NotificationsMenuComponent } from './notifications-menu/notifications-menu.component';
+import { ProfileMenuComponent } from './components/profile-menu/profile-menu.component';
+import { NotificationsMenuComponent } from './components/notifications-menu/notifications-menu.component';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-import { CoinItemLoaderComponent } from './coin-item-loader/coin-item-loader.component';
+import { CoinItemLoaderComponent } from './components/coin-item-loader/coin-item-loader.component';
+import { CryptoChartComponent } from './components/crypto-chart/crypto-chart.component';
 
 @NgModule({
   imports: [
@@ -36,6 +37,8 @@ import { CoinItemLoaderComponent } from './coin-item-loader/coin-item-loader.com
     ProfileMenuComponent,
     NotificationsMenuComponent,
     CoinItemLoaderComponent,
+    CryptoChartComponent,
+    CryptoChartComponent,
   ],
   exports: [
     LoaderComponent,
@@ -47,6 +50,7 @@ import { CoinItemLoaderComponent } from './coin-item-loader/coin-item-loader.com
     ProfileMenuComponent,
     NotificationsMenuComponent,
     CoinItemLoaderComponent,
+    CryptoChartComponent,
   ],
   providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }],
   bootstrap: [CoinsListsComponent],
